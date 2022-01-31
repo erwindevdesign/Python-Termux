36abfe1cb8c5c147f23f550175ccc35eecd46ba9
erwindevdesign <erwinf.sanchez@outlook.com>
2022-01-31 09:52:51 -0600

    subit a repo remoto

---
+++ prueba1.py
@@ -0,0 +1,46 @@
+'''print(2+2)
+print(50-5*3)
+
+print(8/4)
+
+print('l=4, x=7')
+
+mensaje = 'Hi bitch'
+print(mensaje)
+
+nombre=input('¿Como te llamas?')
+n = input("Introduce un numero entero : ")
+print((nombre + "n") * int(n))
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+nombre = input ('Ingresa tu nombre :')
+print (('Hola ' + nombre + ' !')
+
+a = 47
+b=83
+xx=a/b
+print(xx)
+print(type(xx))
+
+x=('this is a text')
+print=x
+'''
+a=2
+b=5
+valor=(a**b)
+print(valor)
+
+print (((3+4)/(2*5))**2)
+
+n = int(input("introduce un número entero : "))
+suma=n*(n+1)/2
+print('La suma de los primeros numeros enteros desde 1 hasta ' + str(n) +' es '+str(suma))
+
+
+horas= float(input('Introduce tus horas de trabajo : '))
+coste=float(input('Imtroduce lo que cobras por hora : '))
+paga = horas * coste
+print('Tu paga es $', paga)
+
